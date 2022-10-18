bc28d89dca14cf640ef42cdcc729d9d6f32cbdb2
rodwnl <qurtks2224@gmail.com>
2022-10-19 05:25:58 +0900

    Image 추가

---
+++ src/main/java/com/example/awstest2/model/Image.java
@@ -0,0 +1,23 @@
+package com.example.awstest2.model;
+
+public class Image {
+    private String image;
+
+    private int nid;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getNid() {
+        return nid;
+    }
+
+    public void setNid(int nid) {
+        this.nid = nid;
+    }
+}
